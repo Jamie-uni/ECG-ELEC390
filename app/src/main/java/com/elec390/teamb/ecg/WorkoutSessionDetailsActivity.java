2b12e00609d2b90b0e3520102527bb9ea850ffd6
Jamie <jamie@stepanian.ca>
2018-03-03 14:28:57 -0500

    Added dummy classes. Added initial UML Diagram.

---
+++ app/src/main/java/com/elec390/teamb/ecg/WorkoutSessionDetailsActivity.java
@@ -0,0 +1,13 @@
+package com.elec390.teamb.ecg;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WorkoutSessionDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workout_session_details);
+    }
+}
