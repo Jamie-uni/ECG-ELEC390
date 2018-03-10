48cd3a4418dad5b21a258f303cd200b0dba43cc1
juliaporfilio <juliaporfilio@gmail.com>
2018-03-10 14:12:28 -0500

    progress so far on the WorkoutHistoryActivity

--- app/src/main/java/com/elec390/teamb/ecg/WorkoutHistoryActivity.java
+++ app/src/main/java/com/elec390/teamb/ecg/WorkoutHistoryActivity.java
@@ -1,18 +1,62 @@
 package com.elec390.teamb.ecg;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class WorkoutHistoryActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class WorkoutHistoryActivity extends AppCompatActivity
+{
+    private DataStorage dataStorage;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout_history);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Workout History");
+
+        // Check if access to external storage is granted
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Request the permission to access external storage
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},0);
+        }
+
+        dataStorage = new DataStorage(this);
+
+        printSessions();
+
     }
+
+    private void printSessions()
+    {
+        String ListView = "";
+        List<SessionEntity> sessions = dataStorage.getSessionList();
+
+        for(int i=0 ; i<sessions.size() ; i++) {
+            ListView += "Session #" + sessions.get(i).sId + "\nStart Time:\n"+ sessions.get(i).mSessionStart
+                    + "\nEnd Time:\n" + sessions.get(i).mSessionEnd + "\nFile Name in ECGData Folder:\n"
+                    + sessions.get(i).mSessionDataFileName + "\nSession Comments:\n"
+                    + sessions.get(i).mSessionComments + "\n";
+        }
+    }
+
+
 }
