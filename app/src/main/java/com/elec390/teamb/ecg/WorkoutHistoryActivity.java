8c430d0b07a50d93fd35281a55855ddefbea3700
svb <stevevanb>
2018-03-11 20:16:14 -0400

    Fixed workout history activity

--- app/src/main/java/com/elec390/teamb/ecg/WorkoutHistoryActivity.java
+++ app/src/main/java/com/elec390/teamb/ecg/WorkoutHistoryActivity.java
@@ -1,61 +1,29 @@
 package com.elec390.teamb.ecg;
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.database.Cursor;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.text.method.ScrollingMovementMethod;
-import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class WorkoutHistoryActivity extends AppCompatActivity
 {
     private DataStorage dataStorage;
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout_history);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Workout History");
-
-        // Check if access to external storage is granted
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED) {
-            // Request the permission to access external storage
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},0);
-        }
-
         dataStorage = new DataStorage(this);
-
         printSessions();
-
     }
-
     private void printSessions()
     {
-        String ListView = "";
         List<SessionEntity> sessions = dataStorage.getSessionList();
-
-        for(int i=0 ; i<sessions.size() ; i++) {
-            System.out.println(sessions.get(i).sId + "      " + sessions.get(i).mSessionStart + "     " + sessions.get(i).mSessionEnd
-                    + "     " + sessions.get(i).mSessionComments + "      " + sessions.get(i).mSessionDataFileName);
-        }
+        // Set ListView adapter to display the toString() of each session in a separate TextView
+        ArrayAdapter<SessionEntity> adapter = new ArrayAdapter<SessionEntity>(this,
+                R.layout.activity_listview, sessions);
+        ListView listView = findViewById(R.id.historyList);
+        listView.setAdapter(adapter);
     }
-
-    
-
 }
