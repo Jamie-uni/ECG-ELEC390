9da6793d289a56cb2a824705c2181b294bb3cf13
svb <stevevanb>
2018-03-23 17:26:31 -0400

    Workout history layout fixes

--- app/src/main/java/com/elec390/teamb/ecg/WorkoutHistoryActivity.java
+++ app/src/main/java/com/elec390/teamb/ecg/WorkoutHistoryActivity.java
@@ -4,9 +4,15 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -20,7 +26,8 @@ import java.io.File;
 import java.io.FileWriter;
 import java.util.List;
 
-public class WorkoutHistoryActivity extends Activity {
+public class WorkoutHistoryActivity extends Activity
+        implements NavigationView.OnNavigationItemSelectedListener {
     private DataStorage dataStorage;
     private List<SessionEntity> sessions;
     private ListView mListView;
@@ -31,8 +38,16 @@ public class WorkoutHistoryActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout_history);
         final Context context = this;
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("Workout History");
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
         dataStorage = new DataStorage(context);
         sessions = dataStorage.getSessionList();
         mListView = (ListView) findViewById(R.id.sessionListView);
@@ -83,31 +98,57 @@ public class WorkoutHistoryActivity extends Activity {
         });
     }
     @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.edit_profile_menu, menu);
-        editMenuItem = menu.findItem(R.id.editProfileButton);
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.workout, menu);
         return true;
     }
-    /* Switch the activity to edit-mode, enable save button and text input */
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle item selection
-        if (item.getItemId() == R.id.editProfileButton) {
-            deleteMode = deleteMode^deleteMode;
-            if (deleteMode == true)
-                Toast.makeText(getApplicationContext(), "Delete Mode", Toast.LENGTH_LONG).show();
-            else
-                Toast.makeText(getApplicationContext(), "Regular Mode", Toast.LENGTH_LONG).show();
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
             return true;
         }
-        else
-            return super.onOptionsItemSelected(item);
+
+        return super.onOptionsItemSelected(item);
     }
-    private void printSessions() {
-        // Set ListView adapter to display the toString() of each session in a separate TextView
-        final ArrayAdapter<SessionEntity> adapter = new ArrayAdapter<SessionEntity>(this,
-                R.layout.activity_listview, sessions);
-        mListView.setAdapter(adapter);
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_Workout) {
+            Log.d("TAG", "Drawer: Workout was selected.");
+            startActivity(new Intent(this,WorkoutActivity.class));
+        }
+        else if (id == R.id.nav_WorkoutHistory) {
+            Log.d("TAG", "Drawer: Session History was selected.");
+            startActivity(new Intent(this,WorkoutHistoryActivity.class));
+        }
+        else if (id == R.id.nav_Settings) {
+            Log.d("TAG", "Drawer: Settings was selected.");
+            //startActivity(new Intent(this,SettingsActivity.class));
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
