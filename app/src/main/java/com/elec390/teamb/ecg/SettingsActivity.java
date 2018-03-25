f97f123b97602ea9947cede6172b11c516c47593
svb <stevevanb>
2018-03-25 14:02:41 -0400

    Settiing activity

--- app/src/main/java/com/elec390/teamb/ecg/SettingsActivity.java
+++ app/src/main/java/com/elec390/teamb/ecg/SettingsActivity.java
@@ -1,8 +1,8 @@
 package com.elec390.teamb.ecg;
-import android.support.v7.app.ActionBar;
+
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -12,79 +12,85 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class SettingsActivity extends AppCompatActivity
-{
-    protected Toolbar tool_bar;
-    protected EditText name_edit_text;
-    protected EditText email_edit_text;
-    protected Button save_button;
-    protected Profile profile;
-    protected SharedPreferenceHelper shared_preference_helper;
-
+public class SettingsActivity extends AppCompatActivity {
+    // Define private member variables
+    private EditText nameEditText, emailEditText;
+    private Button saveButton;
+    private Profile profile;
+    private SharedPreferenceHelper sharedPreferenceHelper;
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Settings");
-
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
-        tool_bar = (Toolbar) findViewById(R.id.toolbar);
-        tool_bar.setTitle("User Profile Information");
-        setSupportActionBar(tool_bar);
-        ActionBar action_bar = getSupportActionBar();
-        action_bar.setDisplayHomeAsUpEnabled(true);
-        shared_preference_helper = new SharedPreferenceHelper(SettingsActivity.this);
-        profile = new Profile(shared_preference_helper.getProfile());
-        name_edit_text = (EditText) findViewById(R.id.nameEditText);
-        email_edit_text = (EditText) findViewById(R.id.emailEditText);
-        name_edit_text.setText(profile.getName(), TextView.BufferType.NORMAL);
-        email_edit_text.setText(profile.getEmail(), TextView.BufferType.NORMAL);
-        name_edit_text.setFocusable(false);
-        email_edit_text.setFocusable(false);
-        save_button = (Button) findViewById(R.id.saveSettingsButton);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        // Use the controller SharedPreferenceHelper to retrieve the profile from SharedPreferences
+        sharedPreferenceHelper = new SharedPreferenceHelper(SettingsActivity.this);
+        profile = new Profile(sharedPreferenceHelper.getProfile());
+        // Setup the EditTexts and save Button
+          nameEditText = findViewById(R.id.nameEditText);
+        emailEditText = findViewById(R.id.emailEditText);
+        nameEditText.setText(profile.getName(), TextView.BufferType.NORMAL);
+        emailEditText.setText(profile.getEmail(), TextView.BufferType.NORMAL);
+        nameEditText.setFocusable(false);
+        emailEditText.setFocusable(false);
+        saveButton = findViewById(R.id.saveButton);
+        saveButton.setVisibility(View.INVISIBLE);
     }
-
+    // Creates the options menu, which contains Edit
     @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        MenuInflater inflate_menu = getMenuInflater();
-        inflate_menu.inflate(R.menu.edit_profile_menu, menu);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.edit_profile_menu, menu);
         return true;
     }
-
+    // Called when an item from the options menu is selected
     @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        switch (item.getItemId())
-        {
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
             case R.id.editProfileButton:
-                Toast.makeText(SettingsActivity.this, "You are now in Edit Mode", Toast.LENGTH_SHORT).show();
-
-                name_edit_text.setFocusableInTouchMode(true);
-                email_edit_text.setFocusableInTouchMode(true);
-                save_button.setVisibility(View.VISIBLE);
-                tool_bar.setTitle("Edit Mode");
+                // Edit is selected, the EditTexts become editable and the save button appears
+                Toast.makeText(SettingsActivity.this, "Switching to edit mode...", Toast.LENGTH_SHORT).show();
+                nameEditText.setFocusableInTouchMode(true);
+                emailEditText.setFocusableInTouchMode(true);
+                saveButton.setVisibility(View.VISIBLE);
+  //              toolBar.setTitle("Edit Profile");
                 return true;
-
             default:
+                // Invoke the superclass
                 return super.onOptionsItemSelected(item);
         }
     }
 
-    public void saveChanges(View view)
-    {
-        Toast.makeText(SettingsActivity.this, "All data entered is being saved.", Toast.LENGTH_SHORT).show();
-
-        profile.setName(name_edit_text.getText().toString());
-        profile.setEmail(email_edit_text.getText().toString());
-        shared_preference_helper.saveProfile(profile);
-        name_edit_text.setFocusable(false);
-        email_edit_text.setFocusable(false);
-        save_button.setVisibility(View.INVISIBLE);
-        tool_bar.setTitle("User Profile Information");
+    // Method called from the xml layouts saveButton onClick. If the user input is valid,
+    // the profile gets changed and stored in SharedPreferences
+    public void saveChanges(View view) {
+        if(checkForValidInput()) {
+            Toast.makeText(SettingsActivity.this, "Saving profile...", Toast.LENGTH_SHORT).show();
+            profile.setName(nameEditText.getText().toString());
+            profile.setEmail(emailEditText.getText().toString());
+            sharedPreferenceHelper.saveProfile(profile);
+            nameEditText.setFocusable(false);
+            emailEditText.setFocusable(false);
+            saveButton.setVisibility(View.INVISIBLE);
+        }
+    }
+    // Checks if the user input in the EditTexts is valid. Constraints for the upper bounds
+    // were made in activity_profile.xml using maxLength
+    private boolean checkForValidInput() {
+        String name = nameEditText.getText().toString();
+        String email = emailEditText.getText().toString();
+        if(name.length() < 3) {
+            Toast.makeText(SettingsActivity.this, "Name is too short", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        else if(email.length() < 6) {
+            Toast.makeText(SettingsActivity.this, "Email is too short", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        else if(!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            Toast.makeText(SettingsActivity.this, "Email is invalid", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        else return true;
     }
 }
